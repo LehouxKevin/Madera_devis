17878e4e8bf0bbbad3199b2ef89452d05b7cba7a
LehouxKevin <kevin.lehoux5@hotmail.fr>
2020-12-28 03:10:47 +0100

    fix front folder structure pages and components

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -4,8 +4,9 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+
 
 
 @NgModule({

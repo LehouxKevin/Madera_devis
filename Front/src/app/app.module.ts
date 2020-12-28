4c14b63fee2b0f97fde1a9dd0f6ca60215752fdd
LehouxKevin <kevin.lehoux5@hotmail.fr>
2020-12-28 02:48:59 +0100

    setting up home and components folder / router and routes / start header structure

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

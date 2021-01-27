e4a366d2256b61fe17ba4e422459d9172ca7147a
Enzo-JURET <juretenzo@gmail.com>
2021-01-27 15:10:34 +0100

    Affichage des icônes de la sidebar selon le type d'utilisateur connecté

--- Front/src/app/pages/home/home.component.ts
+++ Front/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
 import {Router } from '@angular/router';
+import { UtilisateurService } from 'src/app/services/utilisateur.service';
 
 @Component({
   selector: 'app-home',
@@ -9,19 +10,54 @@ import {Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  public utilisateurs:any[] = [];
+  public utilisateur;
+  public typesUtilisateur:any[] = [];
+  public typeUtilisateur;
+
+  constructor(private router:Router, private utilisateurService: UtilisateurService) { }
   ngOnInit(): void 
   {
+    
+    
     this.router.navigate(['accueil']);
     var tokken = JSON.parse(localStorage.getItem('tokken'));
     if (tokken['statut'] == "connecte") {
       document.getElementById('lien_iconeProfil').style.display = "none";
       document.getElementById('Ellipse_connexion2').style.fill = "green";
       document.getElementById('lien_iconeProfil').setAttribute('routerlink', '/devis');
+      this.loadSidebar();
     }
     else {
       document.getElementById('lien_iconeProfil').style.display = "block";
       document.getElementById('lien_iconeProfil2').style.display = "none";
+      document.getElementById('icone_statistiquesCommercial').remove();
+      document.getElementById('icone_synchronisation').remove();
+      document.getElementById('icone_listeGamme').remove();
+      document.getElementById('icone_listeFournisseur').remove();
+      document.getElementById('icone_listeParametres').remove();
+    }
+  }
+
+  async loadSidebar() {
+    var getTokken = JSON.parse(localStorage.getItem('tokken'));
+    this.utilisateur  = await this.utilisateurService.getOneUtilisateurById(getTokken["objet"]);
+
+    // Type utilisateur : Administrateur
+    if (this.utilisateur.typeUtilisateur.slice(23) == 1) {
+      console.log("[Vous êtes connecté en tant qu'Administrateur]");
+    }
+    // Type utilisateur : Commercial
+    else if (this.utilisateur.typeUtilisateur.slice(23) == 2) {
+      document.getElementById('icone_listeGamme').remove();
+      document.getElementById('icone_listeFournisseur').remove();
+      document.getElementById('icone_listeParametres').remove();
+    }
+    // Type utilisateur : Bureau d'études
+    else if (this.utilisateur.typeUtilisateur.slice(23) == 3) {
+      document.getElementById('icone_listeDevis').remove();
+      document.getElementById('icone_statistiquesCommercial').remove();
+      document.getElementById('icone_synchronisation').remove();
     }
   }
 }

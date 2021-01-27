830c333b6138f9bcbe8ee0305aa4252a832545eb
LehouxKevin <kevin.lehoux5@hotmail.fr>
2021-01-27 01:42:33 +0100

    add client form setted up + dynamics form validity + error message while value aren't valid + field value validity checked on sumbit event form

--- Front/src/app/components/ajout-client/ajout-client.component.ts
+++ Front/src/app/components/ajout-client/ajout-client.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-ajout-client',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AjoutClientComponent implements OnInit {
 
+  nom:string = "";
+  prenom:string = "";
+  adresse:string = "";
+  telephone:string = "";
+  mail:string = "";
+
+  // true par défaut pour ne pas que les messages d'erreurs s'affichent à l'arriver sur la page. Dès le submit les vérifications sont faites donc les 
+  // champs qui ne sont pas remplis correctement passent bien à false et l'affichage s'actualise.
+  isNomValide:boolean=true;
+  isPrenomValide:boolean=true;
+  isAdresseValide:boolean=true;
+  isTelephoneValide:boolean=true;
+  isMailValide:boolean=true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(ajoutClient: NgForm) {
+    console.warn(ajoutClient.value);
+    this.nom = ajoutClient.value.nom;
+    this.prenom = ajoutClient.value.prenom;
+    this.adresse = ajoutClient.value.adresse;
+    this.telephone = ajoutClient.value.telephone;
+    this.mail = ajoutClient.value.mail;
+
+    this.nom.length <= 0 || this.nom == null ? this.isNomValide = false : this.isNomValide = true;
+    this.prenom.length <= 0 || this.prenom == null ? this.isPrenomValide = false : this.isPrenomValide = true;
+    this.adresse.length <= 0 || this.adresse == null ? this.isAdresseValide = false : this.isAdresseValide = true;
+    this.telephone.length <= 0 || this.telephone == null ? this.isTelephoneValide = false : this.isTelephoneValide = true;
+    this.mail.length <= 0 || this.mail == null ? this.isMailValide = false : this.isMailValide = true;
+
+    if(this.isNomValide && this.isPrenomValide && this.isAdresseValide && this.isTelephoneValide && this.isMailValide)
+    {
+      console.log("Tout les champs sont correctement remplis");
+    }
+    
   }
 
 }

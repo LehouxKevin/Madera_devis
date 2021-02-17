322a1f179407353e8c74e1ba1172054abe57a0f5
Enzo-JURET <juretenzo@gmail.com>
2021-02-17 15:40:56 +0100

    Ajout de la fonctionnalite de creation d'une coupe de principe

--- Front/src/app/pages/liste-coupes-de-principe/liste-coupes-de-principe.component.ts
+++ Front/src/app/pages/liste-coupes-de-principe/liste-coupes-de-principe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { CoupeDePrincipes } from 'src/app/class/coupe-de-principes';
@@ -11,30 +12,146 @@ import { CoupeDePrincipeService } from 'src/app/services/coupe-de-principe.servi
 })
 export class ListeCoupesDePrincipeComponent implements OnInit, OnDestroy {
 
+  // Variable public utilisée dans les différentes fonctions
   cdp:CoupeDePrincipes;
-
   public coupesDePrincipe:any[] = [];
   public coupeDePrincipe;
+  public display:boolean = true;
+  public typeForm:boolean;
 
+  // Constructeur de la classe ListeCoupesDePrincipeComponent
   constructor(private router:Router, private coupeDePrincipeService: CoupeDePrincipeService) { }
 
+  /**
+   * Fonction permettant d'initialiser les éléments de la page.
+   * S'exécute lors de l'initialisation de la page.
+   * Ne prend aucun paramètre.
+  **/
   ngOnInit(): void {
+    // Ajout de la bordure de suivi dans la sidebar à côté de l'icone des paramètres
     document.getElementById("icone_listeParametres").style.borderLeft = "solid #BCE0FD 5px";
+    // Modification du bouton et du lien du menu des paramètres
     document.getElementById("bouton_coupeDePrincipe").style.border = "solid 2px #1d2932";
     document.getElementById("bouton_coupeDePrincipe").style.color = "#1d2932";
     document.getElementById("bouton_coupeDePrincipe").setAttribute("onclick","return false");
+    // Récupération de la totalité des coupe de principes intérieures
     this.coupeDePrincipeService.getCoupeDePrincipes().pipe(
       map(coupeDePrincipe => coupeDePrincipe['hydra:member'])
     ).subscribe(
+      // On stocke ces valeurs dans une variable public (de type -> tableau)
       coupeDePrincipe => this.coupesDePrincipe = coupeDePrincipe
     );
   }
 
+  /**
+   * Fonction permettant de fermer convenablement la page.
+   * S'exécute lors de la fermeture de la page.
+   * Ne prend aucun paramètre.
+  **/
   ngOnDestroy() {
+    // Suppression de la bordure de suivi de la sidebar qui était à côté de l'icone des paramètres
     document.getElementById("icone_listeParametres").style.borderLeft = "solid #BCE0FD 0px";
   }
 
+  /**
+   * Fonction permettant d'afficher les différentes fonctionnalités du CRUD.
+   * S'exécute lorsque l'on clique sur les différentes boutons de la page.
+   * Ne prend aucun paramètre.
+  **/
   displayCoupeDePrincipe() {
-    console.log("Créer coupe de principe !");
+    // Si la fonction displayCoupeDePrincipe n'a pas encore été exécuté
+    if (this.display) {
+      document.getElementById("displayList").style.display = "none";
+      document.getElementById("menu_parametres").style.display = "none";
+      document.getElementById("grand-titre").textContent = "Création d'une coupe de principe";
+      document.getElementById("displayCreate").style.display = "block";
+      document.getElementById("content").style.paddingLeft = "15rem";
+      document.getElementById("content").style.paddingRight = "15rem";
+      document.getElementById("listeCoupesDePrincipe").style.paddingTop = "6rem";
+      document.getElementById("boutonCoupeDePrincipe").setAttribute("src", 'assets\\icones\\displayList.svg');
+      this.display = false;
+      this.typeForm = true;
+    }
+    // Si la fonction displayCoupeDePrincipe a déjà été exécuté
+    else {
+      if (this.typeForm) {
+        document.getElementById("displayCreate").style.display = "none";
+        document.getElementById("libelle").style.borderLeft = "none";
+        document.getElementById("prix").style.borderLeft = "none";
+        document.getElementById("libelleError").style.display = "none";
+        document.getElementById("prixError").style.display = "none";
+      }
+      else {
+        // À FAIRE -> Reset le formulaire de modification !
+      }
+      document.getElementById("menu_parametres").style.display = "block";
+      document.getElementById("displayList").style.display = "block";
+      document.getElementById("grand-titre").textContent = "Liste des coupes de principe";
+      document.getElementById("content").style.paddingLeft = "3rem";
+      document.getElementById("content").style.paddingRight = "3rem";
+      document.getElementById("listeCoupesDePrincipe").style.paddingTop = "2rem";
+      document.getElementById("boutonCoupeDePrincipe").setAttribute("src", "assets\\icones\\displayCreate.svg");
+      this.display = true;
+    }
   }
+
+  /**
+   * Fonction permettant d'intéragir avec les formulaires de la page.
+   * S'exécute lorsque l'on clique sur les boutons (de type submit) des formulaires de la page.
+   * Paramètres :
+   *  - coupeDePrincipeForm -> Retourne les valeurs saisies dans le formulaire en question.
+   *  - typeform              -> Retourne "true" si le formulaire est celui de la création d'une coupe de principe extérieur et "false" si c'est celui de la modifcation.
+  **/
+ async onSubmit(coupeDePrincipeForm: NgForm, typeForm:boolean) 
+ {
+   // Si le formulaire est celui de la création d'une coupe de principe intérieure
+   if (typeForm) {
+
+     // Initialisation de l'affichage des éléments du formulaire
+     document.getElementById("libelle").style.borderLeft = "none";
+     document.getElementById("prix").style.borderLeft = "none";
+     document.getElementById("libelleError").style.display = "none";
+     document.getElementById("prixError").style.display = "none";
+
+     // Initialisation des variables utilisés lors de la création de la coupe de principe intérieure 
+     var libelle = false;
+     var prix = false;
+     
+     // Test si le libelle de la coupe de principe a bien été renseigné
+     if (coupeDePrincipeForm.value.libelle) {
+       libelle = true;
+     }
+     else {
+       document.getElementById("libelle").style.borderLeft = "solid red 9px";
+       document.getElementById("libelleError").textContent = "Veuillez définir le libelle de votre coupe de principe !"
+       document.getElementById("libelleError").style.display = "block";
+     }
+
+     // Test si le prix de la coupe de principe a bien été renseigné
+     if (coupeDePrincipeForm.value.prix) {
+       prix = true;
+     }
+     else {
+       document.getElementById("prix").style.borderLeft = "solid red 9px";
+       document.getElementById("prixError").textContent = "Veuillez définir le prix de votre coupe de principe !"
+       document.getElementById("prixError").style.display = "block";
+     }
+
+     if (libelle && prix) {
+       this.cdp = new CoupeDePrincipes(coupeDePrincipeForm.value.libelle, coupeDePrincipeForm.value.prix, coupeDePrincipeForm.value.description);
+       if(await this.coupeDePrincipeService.addCoupeDePrincipe(this.cdp))
+       {
+         this.displayCoupeDePrincipe();
+         this.ngOnInit();
+       }
+       else {
+         console.log("Importation échoué !");
+       }
+     }
+   }
+   // Si le formulaire est celui de la modification d'une coupe de principe intérieure
+   else {
+     console.log("Modification de la coupe de principe en question !");
+   }
+ }
 }
--- Front/src/app/services/coupe-de-principe.service.ts
+++ Front/src/app/services/coupe-de-principe.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { CoupeDePrincipes } from '../class/coupe-de-principes';
 import { map, takeUntil, catchError, tap, finalize } from 'rxjs/operators';
@@ -13,22 +13,54 @@ import { map, takeUntil, catchError, tap, finalize } from 'rxjs/operators';
 export class CoupeDePrincipeService {
 
   baseUrl = environment.baseUrlAPI;
-   CoupeDePrincipeApi = '/coupe_de_principes';
- constructor(private http: HttpClient) { }
+  CoupeDePrincipeApi = '/coupe_de_principes';
+  constructor(private http: HttpClient) { }
 
-   getCoupeDePrincipes(): Observable<CoupeDePrincipes[]>
-   {
-     return this.http.get<CoupeDePrincipes[]>(this.baseUrl+this.CoupeDePrincipeApi);
-   }
+  getCoupeDePrincipes(): Observable<CoupeDePrincipes[]>
+  {
+    return this.http.get<CoupeDePrincipes[]>(this.baseUrl+this.CoupeDePrincipeApi);
+  }
 
-   getOneCoupeDePrincipeById(id)
-   {
-     return this.http.get<any[]>(this.baseUrl+this.CoupeDePrincipeApi+"/"+id).toPromise();
-   }
+  getOneCoupeDePrincipeById(id)
+  {
+    return this.http.get<any[]>(this.baseUrl+this.CoupeDePrincipeApi+"/"+id).toPromise();
+  }
 
-     getOneCoupeDePrincipeByICleEtrangere(CleEtrangere)
-     {
-       return this.http.get<any[]>(this.baseUrl+CleEtrangere).toPromise();
-     }
+  getOneCoupeDePrincipeByICleEtrangere(CleEtrangere)
+  {
+  return this.http.get<any[]>(this.baseUrl+CleEtrangere).toPromise();
+  }
 
+  addCoupeDePrincipe(coupeDePrincipe:CoupeDePrincipes)
+  {
+    return this.http.post<CoupeDePrincipes>(this.baseUrl+this.CoupeDePrincipeApi,coupeDePrincipe)
+    .pipe(
+      catchError(this.handleError)
+    ).toPromise().then(data => {
+        // Retourne true si la finition intérieure a un id dans la bdd, si elle en a un c'est qu'elle a bien été inséré
+        if(data.id > 0)
+        {
+          return true;
+        }
+        else {
+          return false;
+        }
+    });
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
 }
